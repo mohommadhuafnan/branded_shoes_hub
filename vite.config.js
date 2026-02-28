2f62495a4db5f09a236884364d27fb37b6ccf60b
mohommadhuafnan <mohommadhuafnan756@gmail.com>
2026-02-28 13:49:18 +0530

    Add GitHub Pages workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '/branded_shoes_hub/',
   plugins: [react()],
 })
